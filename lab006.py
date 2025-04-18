36cd4325ba95c99eae20f832e980585a1d0b89fa
HP SSD <rahullikhar8329@gmail.com>
2025-04-18 09:00:48 +0530

    initial commit 2

---
+++ lab006.py
@@ -0,0 +1,19 @@
+# input - 2
+# 2*1 = 2
+# 2*2 = 4
+# 2*3 = 6
+# 2*20 = 40
+
+# 1. - Input User
+# 2. - Rough Logic
+# 3. - Test Code
+
+user_input = int(input("Enter your number!"))
+print("printing the table")
+
+print(f"{user_input}*1={user_input*1}")
+print(f"{user_input}*2={user_input*2}")
+print(f"{user_input}*3={user_input*3}")
+print(f"{user_input}*4={user_input*4}")
+print(f"{user_input}*5={user_input*5}")
+print(f"{user_input}*6={user_input*6}")
