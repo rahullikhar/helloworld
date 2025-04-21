be8739712e9c067a28a66cd63408a8a72e6c7f83
HP SSD <rahullikhar8329@gmail.com>
2025-04-21 08:14:18 +0530

    initial commit 3

---
+++ Day4/lab010.py
@@ -0,0 +1,24 @@
+s = "hello word"
+print(s.capitalize())
+
+s = 'HELLO World'
+print(s.casefold())
+
+s = 'hello'
+print(s.center(20, '*'))
+
+s = "hello worls"
+print(s.count('o'))
+
+s = 'hello world'
+print(s.encode())
+
+s = "hello world"
+print(s.index('world'))
+
+s = 'hello world'
+print(s.endswith('world'))
+
+s = 'hello\tworld'
+print(s.expandtabs(4))
+
