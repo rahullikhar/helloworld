be457ae3a9f99b9a49ae30eadae8829504737855
HP SSD <rahullikhar8329@gmail.com>
2025-05-12 15:49:33 +0530

    initial commit 5

---
+++ Day5/day 13.py
@@ -0,0 +1,13 @@
+#list- collection of items
+# shoping list
+my_list=["milk","oranges","banana","apple"]
+print(my_list)
+print(my_list[0])
+print(my_list[1])
+print(my_list[2])
+print(len(my_list))
+print(my_list)
+print(my_list[0])
+print(my_list[1])
+print(my_list[2])
+print(len(my_list))
